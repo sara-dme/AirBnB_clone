320bc52ec34ebcd2dccf452519b402586b4661be
sara-dme <s.daoumy@gmail.com>
2023-10-15 09:14:41 -0700

    remove file.json and ....swo file

--- console.py
+++ console.py
@@ -19,12 +19,6 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
-
     def do_EOF(self, line):
         """EOF (End Of File) to exit program"""
         print()
@@ -37,79 +31,95 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """an empty line doesn't do anything on Enter"""
         pass
+    
+    def errors(self, line, cmd):
+        """manages error message for user input"""
 
-    def do_create(self, arg):
-        """create an instance."""
-        if arg is None or arg == "":
+        cls_list = [
+                "BaseModel", "User", "State", "City",
+                "Amenity", "Place", "Review"
+                ]
+        cmd_list = ["create", "show", "all", "destroy", "update"]
+
+        if line == "":
             print("** class name missing **")
-        elif arg not in HBNBCommand.classes:
+            return 1
+        args = line.split()
+        if args[0] not in cls_list and cmd in cmd_list:
             print("** class doesn't exist **")
-        else:
-            new = HBNBCommand.classes[arg]()
-            storage.save()
-            print(new.id)
+            return 1
+        elif cmd in ["create", "all"]:
+            return
+        if len(args) < 2 and cmd in ["show", "destroy", "update"]:
+            print("** instance id missing **")
+            return 1
+        instances_dict = storage.all()
+        args = self.remove_quot(args)
+        k = args[0] + '.' + args[1]
+        if k not in instances_dict and cmd in ["show", "destroy", "update"]:
+            print("** no instance found **")
+            return 1
+        elif cmd in ["show", "destroy"]:
+            return 0
 
-    def do_show(self, arg):
-        """Print the string rpresentation of an instance"""
-        if arg == "" or arg is None:
-            print("** class name missing **")
-        else:
-            a = arg.split(' ')
-            if a[0] not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-            elif len(a) == 1:
-                print("** instance id missing **")
-            else:
-                line = "{}.{}".format(a[0], a[1])
-                if line not in storage.all():
-                    print("** no instance found **")
-                else:
-                    print(line)
+        if len(args) < 3 and cmd == "update":
+            print("** attribute name missing **")
+            return 1
+        if len(args) < 4 and cmd == "update":
+            print("** value missing **")
+            return 1
+        return 0
 
-    def do_destroy(self, arg):
-        """ Delete an instance :destroy <class> <id>"""
-        if arg == "" or arg is None:
-            print("** class name missing **")
-        else:
-            a = arg.split(' ')
-            if a[0] not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-            elif len(a) == 1:
-                print("** instance id missing **")
-            else:
-                line = "{}.{}".format(a[0], a[1])
-                if line not in storage.all():
-                    print("** no instance found **")
-                else:
-                    del storage.all()[line]
-                    storage.save()
-
-    def do_all(self, arg):
-        """Prints all string representation of all instances"""
-        print_list = []
-        instance_dict = storage.all()
-        if arg == "":
-            print_list = [str(i) for i in instance_dict.values()]
-            print(print_list)
+    def do_create(self, line):
+        """create an instance."""
+        if (self.errors(line, "create") == 1):
             return False
-        if arg:
-            arg = arg.split(' ')[0]
-            if arg not in HBNBCommand.classes:
-                print("")
-            else:
-                for k, v in storage._FileStorage__objects.items():
-                    if k.split('.')[0] == arg:
-                        print_list.append(str(v))
-        else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
-        print(print_list)
+        instance = eval(line)()
+        instance.save()
+        print(instance.id)
 
-    def do_count(self, args):
+    def do_show(self, line):
+        """Print the string rpresentation of an instance"""
+        if (self.errors(line, "show") == 1):
+            return False
+
+        args = line.split()
+        instances_dict = storage.all()
+        args = self.remove_quot(args)
+        print(instances_dict[args[0] + '.' + args[1]])
+
+    def do_destroy(self, line):
+        """ Delete an instance :destroy <class> <id>"""
+        if (self.errors(line, "destroy") == 1):
+            return False
+        instances_dict = storage.all()
+        args = line.split()
+        args = self.remove_quot(args)
+        del instances_dict[args[0] + '.' + args[1]]
+        storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances"""
+        d_list = []
+        instances_dict = storage.all()
+        if line == "":
+            d_list = [str(i) for i in instances_dict.values()]
+            print(d_list)
+            return False
+        args = line.split()
+        if (self.errors(line, "all") == 1):
+            return False
+        for i in instances_dict.values():
+            if i.__class__.__name__ == args[0]:
+                d_list.append(str(i))
+        print(d_list)
+
+    def do_count(self, cls_name):
         """This counts the instance of a class"""
         num_inst = 0
-        for k, v in storage._FileStorage__objects.items():
-            if args == k.split('.')[0]:
+        instances_dict = storage.all()
+        for inst in instances_dict.values():
+            if inst.__class__.__name__ == cls_name:
                 num_inst += 1
         print(num_inst)
 
@@ -120,49 +130,21 @@ class HBNBCommand(cmd.Cmd):
                 arg[i] = arg[i].replace('"', "").replace("'", "")
         return arg
 
-    def do_update(self, arg):
+    def do_update(self, line):
         """Update " certain object with new info"""
-        instance_dict = storage.all()
+        if (self.errors(line, "update") == 1):
+            return False
 
-        if arg == "" or arg is None:
-            print("** class name missing **")
-        else:
-            a = arg.split(' ')
-            a = self.remove_quot(a)
-            if a[0] not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
+            args = line.split()
+            instances_dict = storage.all()
+            args = self.remove_quot(args)
+            x = args[0] + '.' + args[1]
+            new_key = args[2]
+            if args[3].isdigit():
+                new_value = eval(args[3])
             else:
-                classname = a[0]
-
-            if len(a) == 1:
-                print("** instance id missing **")
-                return
-            else:
-                uid = a[1]
-
-            key = classname + "." + uid
-
-            if key not in storage.all():
-                print("** no instance found **")
-                return
-
-            if len(a) == 2:
-                print("** attribute name missing **")
-                return
-
-            if len(a) == 3:
-                try:
-                    type(eval(a[2])) != dict
-                except NameError:
-                    print("** value missing **")
-                    return
-            new_attr = a[2]
-            if a[3].isdigit():
-                new_val = eval(a[3])
-            else:
-                new_val = a[3]
-            setattr(instance_dict[key], new_attr, new_val)
+                new_value = args[3]
+            setattr(instances_dict[x], new_key, new_value)
             storage.save()
 
 
